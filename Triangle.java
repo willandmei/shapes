d41b581af8284eb29bfa93400af34ee15a6019db
Mccarthyw <willandmei@email.com>
2023-06-21 22:22:31 -0700

    Added Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle extends Shape {
+	private Point a;
+	private Point b;
+	private Point c;
+	
+	public Triangle(Point a, Point b, Point c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
